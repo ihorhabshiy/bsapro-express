a9a490d5e393761b44643826e3b607d7253d6280
Ihor Habshiy <ihorhabshiy@gmail.com>
2017-02-12 19:15:12 +0200

    cluster first try

--- app/common/socketService.js
+++ app/common/socketService.js
@@ -5,17 +5,17 @@ class SocketService {
 		io.on('connection', function (socket) {
 			console.log('user connected', socket.id);
 			
-			socket.on('todo:added', function (todo) {
-				socket.broadcast.emit('todo:added', todo);
-			});
+			// socket.on('todo:added', function (todo) {
+			// 	socket.broadcast.emit('todo:added', todo);
+			// });
 
-			socket.on('todo:updated', function (todo) {
-				socket.broadcast.emit('todo:updated', todo);
-			});
+			// socket.on('todo:updated', function (todo) {
+			// 	socket.broadcast.emit('todo:updated', todo);
+			// });
 
-			socket.on('todo:deleted', function (todoId) {
-				socket.broadcast.emit('todo:deleted', todoId);
-			});
+			// socket.on('todo:deleted', function (todoId) {
+			// 	socket.broadcast.emit('todo:deleted', todoId);
+			// });
 		});
 	}
 
